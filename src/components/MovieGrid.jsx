9561c2ad3bb773973a5598e88f498a6ffc58a282
Kai <ali.rf.kashani@gmail.com>
2021-11-26 12:17:18 -0500

    created search functionality

--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -1,9 +1,12 @@
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router'
-import tmbdApi, { category, movieType, tvType } from '../api/tmdbApi'
+import { useState, useEffect, useCallback } from 'react'
+import { useHistory, useParams } from 'react-router'
 
 import MovieCard from './MovieCard'
 import { OulineButton } from './Button'
+import Input from './Input'
+import Button from './Button'
+
+import tmbdApi, { category, movieType, tvType } from '../api/tmdbApi'
 
 const MovieGrid = (props) => {
 	const [items, setItems] = useState([])
@@ -68,6 +71,9 @@ const MovieGrid = (props) => {
 
 	return (
 		<>
+			<div className='section mb-3'>
+				<MovieSearch category={props.category} keyword={keyword} />
+			</div>
 			<div className='movie-grid'>
 				{items.map((item, i) => (
 					<MovieCard category={props.category} item={item} key={i} />
@@ -81,6 +87,43 @@ const MovieGrid = (props) => {
 				</div>
 			) : null}
 		</>
+	)
+}
+
+const MovieSearch = (props) => {
+	const history = useHistory()
+	const [keyword, setKeyword] = useState(props.keyword ? props.keyword : '')
+	const goToSearch = useCallback(() => {
+		if (keyword.trim().length > 0) {
+			history.push(`/${category[props.category]}/search/${keyword}`)
+		}
+	}, [keyword, props.category, history])
+
+	useEffect(() => {
+		const enterEvent = (e) => {
+			e.preventDefault()
+			if (e.keyCode === 13) {
+				goToSearch()
+			}
+		}
+		document.addEventListener('keyup', enterEvent)
+		return () => {
+			document.removeEventListener('keyup', enterEvent)
+		}
+	}, [keyword, goToSearch])
+
+	return (
+		<div className='movie-search'>
+			<Input
+				type='text'
+				placeholder='Seach here ...'
+				value={keyword}
+				onChange={(e) => setKeyword(e.target.value)}
+			/>
+			<Button className='small' onClick={goToSearch}>
+				Search
+			</Button>
+		</div>
 	)
 }
 
